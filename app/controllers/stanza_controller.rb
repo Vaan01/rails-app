7c0b8ded5fcf078509b6115f1f7e8e39ea458718
Vaan01 <114622710+Vaan01@users.noreply.github.com>
2023-06-10 21:11:52 +0200

    Update stanza_controller.rb

--- app/controllers/stanza_controller.rb
+++ app/controllers/stanza_controller.rb
@@ -1,6 +1,8 @@
 class StanzaController < ApplicationController
     def index
-        Room.new(id: 1, nome: "Stanza Romanticismo", xleft:"1", xright:"1", ytop:"1", ybottom:"1").save    
+        if Room.where(id:1).empty?
+           Room.new(id: 1, nome: "Stanza Romanticismo", xleft:"1", xright:"1", ytop:"1", ybottom:"1").save    
+        end
 
         render :json => {"stanze" => Room.all}
     end
