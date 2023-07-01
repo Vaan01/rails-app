a5fc0688f03ea88dc9a708463e60f099056c74f5
Uri <urimick383@gmail.com>
2023-07-02 00:05:43 +0200

    Stanze Dati

--- app/controllers/stanza_controller.rb
+++ app/controllers/stanza_controller.rb
@@ -1,23 +1,23 @@
 class StanzaController < ApplicationController
     def index
         if Room.where(id:1).empty?
-           Room.new(id: 1, nome: "Stanza Romanticismo", xleft:"1", xright:"1", ytop:"1", ybottom:"1").save    
+           Room.new(id: 1, nome: "Stanza Romanticismo", xleft:"1076", xright:"1765", ytop:"412", ybottom:"832").save    
         end
 
         if Room.where(id:2).empty?
-            Room.new(id: 2, nome: "Stanza Impressionismo", xleft:"2", xright:"2", ytop:"2", ybottom:"2").save 
+            Room.new(id: 2, nome: "Stanza Impressionismo", xleft:"45", xright:"1772", ytop:"49", ybottom:"413").save 
         end
 
         if Room.where(id:3).empty?
-            Room.new(id: 3, nome: "Stanza Neoclassicismo", xleft:"3", xright:"3", ytop:"3", ybottom:"3").save 
+            Room.new(id: 3, nome: "Stanza Neoclassicismo", xleft:"44", xright:"707", ytop:"415", ybottom:"838").save 
         end
 
         if Room.where(id:4).empty?
-            Room.new(id: 4, nome: "Stanza Futurismo", xleft:"4", xright:"4", ytop:"4", ybottom:"4").save 
+            Room.new(id: 4, nome: "Stanza Futurismo", xleft:"57", xright:"713", ytop:"835", ybottom:"1253").save 
         end
 
         if Room.where(id:5).empty?
-            Room.new(id: 5, nome: "Stanza Cubismo", xleft:"5", xright:"5", ytop:"5", ybottom:"5").save 
+            Room.new(id: 5, nome: "Stanza Cubismo", xleft:"1077", xright:"1760", ytop:"845", ybottom:"1256").save 
         end
 
         render :json => {"stanze" => Room.all}
