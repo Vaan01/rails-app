2b31ce11ab92411ca4368c0f6c0025ed7aaa35fa
Vaan01 <michael.pasquali@student.univaq.it>
2023-07-02 09:47:49 +0200

    CW Config Fog 3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,10 @@ CarrierWave.configure do |config|
     provider:              'AWS',                                         # required
     aws_access_key_id:     'AKIASBGGNJXLI4UOWDDO',                        # required unless using use_iam_profile
     aws_secret_access_key: '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',    # required unless using use_iam_profile
-    use_iam_profile:       false,                                         # optional, defaults to false
     region:                'eu-north-1',                                  # optional, defaults to 'us-east-1'
-    host:                  'http://unibucketapi.s3-website.eu-north-1.amazonaws.com',                              # optional, defaults to nil
-    endpoint:              'https://unibucketapi.s3-website.eu-north-1.amazonaws.com:8080'                  # optional, defaults to nil
   }
   config.fog_directory  = 'unibucketapi'                                        # required
-  config.fog_public     = true                                                  # optional, defaults to true
+  config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   # For an application which utilizes multiple servers but does not need caches persisted across requests,
   # uncomment the line :file instead of the default :storage.  Otherwise, it will use AWS as the temp cache store.
